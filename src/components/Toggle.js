42272e1fda99695ccecfee62c3372cc5a4c0f8d7
Justin Luciano <justinlluciano@gmail.com>
2022-01-12 21:01:14 -0800

    commit tree 5ae55656c160eb25498d4d6f30d168b93b20e0e8

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setOn] = useState(false);
+  return (
+    <button
+      style={isOn ? { background: "white" } : { background: "red" }}
+      onClick={() => setOn(!isOn)}
+    >
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
